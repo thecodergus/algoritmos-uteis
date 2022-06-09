0ad0139afb311a7b2b5537f9599e4df9795f2aec
gusscamargo <guga.power@hotmail.com>
2022-06-09 18:32:55 -0300

    Alterando forma como as responsabilidades são distribuidas

--- rs/inputline_i64.rs
+++ rs/inputline_i64.rs
@@ -8,9 +8,13 @@ fn inputline() -> String {
 	return input_line.to_string().replace("\n", "");
 }
 
+fn convert_string_to_i64(s: String) -> i64 {
+	return s.trim()
+			.parse::<i64>()
+			.expect("an error happed in convertion Stringo to Int64");
+}
+
 fn inputline_i64() -> i64 {
-    return inputline()
-            .trim()
-            .parse::<i64>()
-            .expect("an error happed in convertion Stringo to Int64");
+    return convert_string_to_i64(inputline());
+            
 }
