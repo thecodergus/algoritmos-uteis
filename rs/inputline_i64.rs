1e894a41d20e7ca26699f6daa8441ed7c539a0bc
gusscamargo <guga.power@hotmail.com>
2022-06-09 18:35:06 -0300

    Padronizando

--- rs/inputline_i64.rs
+++ rs/inputline_i64.rs
@@ -8,13 +8,14 @@ fn inputline() -> String {
 	return input_line.to_string().replace("\n", "");
 }
 
-fn convert_string_to_i64(s: String) -> i64 {
-	return s.trim()
+fn string_to_i64(string: String) -> i64 {
+	return string
+			.trim()
 			.parse::<i64>()
 			.expect("an error happed in convertion Stringo to Int64");
 }
 
 fn inputline_i64() -> i64 {
-    return convert_string_to_i64(inputline());
+    return string_to_i64(inputline());
             
 }
