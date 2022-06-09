d9b7614c1f9e73dad4b17c0ac6d1800f0c0aeea2
gusscamargo <guga.power@hotmail.com>
2022-06-08 22:59:43 -0300

    inputline de um vetor de inteiros

---
+++ rs/inputline_vector_i64.rs
@@ -0,0 +1,35 @@
+fn inputline() -> String {
+	let mut input_line = String::new();
+
+	std::io::stdin()
+		.read_line(&mut input_line)
+		.expect("failed to read from stdin");
+
+	return input_line.to_string().replace("\n", "");
+}
+
+fn splitline(string: String, splitter: &str) -> Vec<String> {
+	let itens: Vec<&str> = string.split(splitter).collect();
+
+	let mut result: Vec<String> = Vec::new();
+
+	for i in itens {
+		result.push(i.to_string().replace("\n", ""));
+	}
+	
+	return result;
+}
+
+fn convert_vector_string_to_i64(vetor: Vec<String>) -> Vec<i64> {
+	let mut result: Vec<i64> = Vec::new();
+
+	for i in vetor {
+		result.push(i.to_string().parse::<i64>().unwrap());
+	}
+
+	return result;
+}
+
+fn inputline_vector_i64() -> Vec<i64> {
+	return convert_vector_string_to_i64(splitline(inputline(), " "));
+}
