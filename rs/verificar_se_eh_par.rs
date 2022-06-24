e574d058fb6f617e6496a3d6e00822688b050f12
gusscamargo <guga.power@hotmail.com>
2022-06-23 21:58:36 -0300

    Melhorando algoritmo de descobrir se numero é par ou impar

--- rs/verificar_se_eh_par.rs
+++ rs/verificar_se_eh_par.rs
@@ -1,6 +1,3 @@
-fn verificar_se_eh_par(num: String) -> bool {
-    let last_c: char = num.chars().last().unwrap();
-    let pares: Vec<char> = Vec::from(['0', '2', '4', '6', '8']);
-    
-    return pares.contains(&last_c);
+fn verificar_se_eh_par(num: i64) -> bool {
+    !((num & 1) == 1)
 }
