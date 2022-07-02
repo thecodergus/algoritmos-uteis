cc707eda55c97a66a57dcd4eb516098f6ee3616a
gusscamargo <guga.power@hotmail.com>
2022-07-02 11:04:36 -0300

    Ajustes funcao

--- cpp/eh_potencia2.cpp
+++ cpp/eh_potencia2.cpp
@@ -1,5 +1,10 @@
 bool eh_potencia2(int x){
     /* First x in the below expression is
       for the case when x is 0 */
-    return x && (!(x & (x - 1)));
+    return !(x & 1);
+}
+
+bool eh_impar(int x)
+{
+  return x & 1;
 }
