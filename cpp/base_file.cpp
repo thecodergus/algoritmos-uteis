9d37a40c729cb4f130fa9de6359ab85799d87ca9
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-07-04 11:31:59 -0300

    criação de um base file pro c++

---
+++ cpp/base_file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define F(x) for(int i = 0; i < x; i++)
+#define pb push_back
+#define Fe(x) for(auto i : x)
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    std::cin.tie(0);
+
+
+    return 0;
+}
