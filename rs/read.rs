873a3a31627d026630716e539eb0e1e5f6166403
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-07-04 09:13:51 -0300

    Ajustes

--- rs/read.rs
+++ rs/read.rs
@@ -30,3 +30,14 @@ macro_rules! read_vec {
             .collect::<Vec<$type>>();
     };
 }
+ 
+
+fn main() {
+	read!(x as u32);
+	read!(y as f64);
+	read!(z as char);
+	println!("{} {} {}", x, y, z);
+
+	read_vec!(v as u32); // Reads space separated integers and stops when newline is encountered.
+	println!("{:?}", v);
+}
