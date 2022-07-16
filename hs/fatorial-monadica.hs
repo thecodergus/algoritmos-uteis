b43ee6aa017d100c4aaa83df2f80446fd1551106
gusscamargo <guga.power@hotmail.com>
2022-07-15 21:27:45 -0300

    incremento

--- hs/fatorial-monadica.hs
+++ hs/fatorial-monadica.hs
@@ -1,11 +1,11 @@
-mult :: Maybe Integer -> Maybe Integer -> Maybe Integer
-mult _ Nothing = Nothing
-mult Nothing _ = Nothing
-mult (Just a) (Just b) = Just $ a * b
+mult_maybe :: Maybe Integer -> Maybe Integer -> Maybe Integer
+mult_maybe _ Nothing = Nothing
+mult_maybe Nothing _ = Nothing
+mult_maybe (Just a) (Just b) = Just $ a * b
 
 fat :: Integer -> Maybe Integer
 fat n 
     | n == 0 = Just 1
     | n < 0 = Nothing
     | otherwise = do
-                    mult (Just n) (fat(n - 1))
+                    mult_maybe (Just n) (fat(n - 1))
