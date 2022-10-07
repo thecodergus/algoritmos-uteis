7fcb823ef16cdef0ce6017520fb5688c8261e210
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-07 09:22:29 -0300

    Corrigindo e incrementando

--- jl/gauss_seidel.jl
+++ jl/gauss_seidel.jl
@@ -1,18 +1,50 @@
-function gauss_seidel(a::Matrix{Number}, x::Vector{Number}, b::Vector{Number}, iter::Int64=1000)::Vector{Number}
+using IterativeSolvers
+
+function gauss_seidel1(a::Matrix{Number}, x::Vector{Number}, b::Vector{Number}, iter::Int64=1000)::Vector{Number}
     n::Int64 = length(b)
 
     for _ = 1:iter
         for i = 1:n
-            x[i] = (b[i] - sum(a[i, :]' .* x) + a[i, i] * x[i]) / a[i, i]
+			# Como tava ontem:
+            # x[i] = (b[i] - sum((a[i, :]' .* x)[:, 1]) + (a[i, i] * x[i])) / a[i, i]
+			# Como ajustei hoje
+			x[i] = (b[i] - ((a[i, :]' * x) - (a[i, i] * x[i]))) / a[i, i]
         end
     end
 
     return x
 end
 
-a::Matrix{Number} = [4 1 2; 3 5 1; 1 1 3]
-b::Vector{Number} = [4, 7, 3]
-x::Vector{Number} = zeros(3)
+function gauss_seidel2(a::Matrix{Number}, x::Vector{Number}, b::Vector{Number}, iter::Int64=1000)::Vector{Number}
+	n = length(b)
 
-println(x)
-println(gauss_seidel(a, b, x))
+	for _ = 1:iter
+		for j = 1:n
+			d = b[j]
+
+			for i = 1:n
+				if i != j
+					d -= a[j, i] * x[i]
+				end
+			end
+			x[j] = d / a[j, j]
+		end
+	end
+
+	return x
+end
+
+# Os 3 algoritmos
+function main()
+	a::Matrix{Number} = [4 1 2; 3 5 1; 1 1 3]
+	b::Vector{Number} = [4, 7, 3]
+	x::Vector{Number} = zeros(3)
+
+	iter = 100
+	
+	println(gauss_seidel1(a, b, x, iter))
+	println(gauss_seidel2(a, b, x, iter))
+	println(IterativeSolvers.gauss_seidel!(x, a, b; maxiter = iter))
+end
+
+main()
