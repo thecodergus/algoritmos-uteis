98152df4d2e7ec18dfd02b79719ba8c887383d3a
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-07-26 15:32:27 -0300

    Ajustes

--- bash/instalar_docker.sh
+++ bash/instalar_docker.sh
@@ -4,31 +4,25 @@
 RED='\033[0;31m'
 NO_COLOR='\033[0m'
 
-# Verifica se o script está sendo executado como superusuário
-if [ "$EUID" -ne 0 ]; then
-    echo -e "${RED}Por favor, execute este script como superusuário (sudo)${NO_COLOR}"
-    exit 1
-fi
-
 # Removendo Pacotes de versões anteriores
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
     echo -e "$RED Removendo $pkg $NO_COLOR"
-    apt remove -y $pkg; 
+    sudo apt remove -y $pkg; 
 done
 
 # Atualizando os pacotes
 echo -e "$RED -> Atualizando os pacotes $pkg $NO_COLOR"
-apt update
+sudo  apt update
 
 # Instalando dependencias minimas
 echo -e "$RED -> Instalando dependencias minimas $pkg $NO_COLOR"
-apt install -y ca-certificates curl gnupg
+sudo  apt install -y ca-certificates curl gnupg
 
 # Adicionando chave Docker GPG
 echo -e "$RED -> Adicionando chave Docker GPG $pkg $NO_COLOR"
-install -m 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
+sudo  chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
@@ -38,11 +32,11 @@ echo \
 
 # Atualizando pacotes novamente
 echo -e "$RED -> Atualizando pacotes novamente $pkg $NO_COLOR"
-apt update
+sudo apt update
 
 # Instalando o docker em si
 echo -e "$RED -> Instalando o docker em si $pkg $NO_COLOR"
-apt install -y docker-ce \
+sudo  apt install -y docker-ce \
             docker-ce-cli \
             containerd.io \
             docker-buildx-plugin \
@@ -50,6 +44,6 @@ apt install -y docker-ce \
 
 # Criando grupo de usuarios docker
 echo -e "$RED -> Criando grupo de usuarios docker $pkg $NO_COLOR"
-groupadd docker
-usermod -aG docker $USER
+sudo  groupadd docker
+sudo  usermod -aG docker $USER
 newgrp docker
