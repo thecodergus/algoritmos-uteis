1f9c2e857beb092686c1492e4aa776815cfaf312
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-08-30 10:26:37 -0300

    Adicionando função para lidar com envs

---
+++ cpp/get_env_or.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+
+std::string get_env_or(const std::string& key, const std::string& default_value) {
+  const char* val = std::getenv(key.c_str());
+  if (!val) {
+    return default_value;
+  }
+
+  return std::string(val);
+}
