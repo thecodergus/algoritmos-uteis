6cbe2385c438c112d34cea93b828360837392899
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-08-31 17:06:03 -0300

    Arrumando algoritmo

--- bash/instalar_protoc.sh
+++ bash/instalar_protoc.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # Versão do protobuf
-VERSAO=24.2
+VERSAO="24.2"
 
 # Salvando a arquitetura do sistema
-ARQUITETURA=$(uname -i)
+ARQUITETURA="$(uname -i)"
 
 # Baixa o arquivo ZIP do Protobuf para a arquitetura do sistema atual
-wget https://github.com/protocolbuffers/protobuf/releases/download/v$VERSAO/protoc-$VERSAOlinux-$ARQUITETURA.zip
+wget https://github.com/protocolbuffers/protobuf/releases/download/v$VERSAO/protoc-$VERSAO-linux-$ARQUITETURA.zip
 
 # Descompacta o arquivo ZIP baixado em um diretório chamado 'protoc'
 unzip protoc-$VERSAO-linux-$ARQUITETURA.zip -d protoc
