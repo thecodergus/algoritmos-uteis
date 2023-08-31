32d9c1fe733ef7be0c19d3ab03264af08055e544
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-08-31 15:43:43 -0300

    Melhorando script

--- bash/instalar_protoc.sh
+++ bash/instalar_protoc.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 
-wget https://github.com/protocolbuffers/protobuf/releases/download/v24.2/protoc-24.2-linux-$(uname -i).zip
-unzip protoc-24.2-linux-x86_64.zip -d protoc
+# Versão do protobuf
+VERSAO=24.2
+
+# Salvando a arquitetura do sistema
+ARQUITETURA=$(uname -i)
+
+# Baixa o arquivo ZIP do Protobuf para a arquitetura do sistema atual
+wget https://github.com/protocolbuffers/protobuf/releases/download/v$VERSAO/protoc-$VERSAOlinux-$ARQUITETURA.zip
+
+# Descompacta o arquivo ZIP baixado em um diretório chamado 'protoc'
+unzip protoc-$VERSAO-linux-$ARQUITETURA.zip -d protoc
+
+# Move o executável 'protoc' para o diretório /usr/local/bin/
 sudo mv protoc/bin/protoc /usr/local/bin/
+
+# Move os arquivos de inclusão para o diretório /usr/local/include/
 sudo mv protoc/include/* /usr/local/include/
