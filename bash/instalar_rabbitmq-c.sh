f0aca69480db1530b2f07915089620f94cb526c3
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-09-01 09:23:02 -0300

    instalar rabbitmq c

---
+++ bash/instalar_rabbitmq-c.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# Atualiza a lista de pacotes disponíveis no repositório do sistema
+sudo apt update
+
+# Instala as dependências necessárias para compilar e instalar a biblioteca rabbitmq-c
+sudo apt install -y \
+    cmake \
+    wget \
+    tar \
+    make
+
+# Baixa a versão 0.13.0 da biblioteca rabbitmq-c do GitHub
+wget https://github.com/alanxz/rabbitmq-c/archive/refs/tags/v0.13.0.tar.gz
+
+# Descompacta o arquivo tar.gz baixado
+tar -zxvf v0.13.0.tar.gz
+
+# Entra no diretório do código-fonte descompactado
+cd rabbitmq-c-0.13.0/
+
+# Cria um diretório para a construção do projeto e entra nele
+mkdir build
+cd build
+
+# Executa o CMake para preparar a compilação do projeto
+cmake ..
+
+# Compila a biblioteca
+make
+
+# Instala a biblioteca rabbitmq-c no sistema
+sudo make install
