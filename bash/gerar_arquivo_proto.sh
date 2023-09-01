d2ccf833d43380e45e7caa75f17d4e6e5aea409a
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-09-01 14:52:27 -0300

    Add

--- bash/gerar_arquivo_proto.sh
+++ bash/gerar_arquivo_proto.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+# Diretório onde o arquivo .proto está localizado
+PROTO_DIR="./protos"
+
 # Nome do arquivo .proto
-PROTO_FILE="estrutura_servidor_rpc.proto"
+PROTO_FILE="route_guide.proto"
 
-# Gerar código C++ a partir do arquivo .proto
-protoc -I=$SRC_DIR --cpp_out=$DST_DIR $SRC_DIR/$PROTO_FILE.proto
+# Diretórios de saída para arquivos de cabeçalho e código-fonte
+INCLUDE_DIR="./include"
+SRC_DIR="./src"
 
-# Mover arquivos de cabeçalho para a pasta 'include'
-mv $DST_DIR/$PROTO_FILE.pb.h $DST_DIR/include/
+# Verificar se os diretórios de saída existem; caso contrário, criá-los
+mkdir -p $INCLUDE_DIR
+mkdir -p $SRC_DIR
 
-# Mover arquivos de código-fonte para a pasta 'src'
-mv $DST_DIR/$PROTO_FILE.pb.cc $DST_DIR/src/
+# Gerar arquivos de código-fonte gRPC
+protoc -I $PROTO_DIR --grpc_out=$SRC_DIR --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $PROTO_DIR/$PROTO_FILE
+
+# Gerar arquivos de código-fonte C++ do Protocol Buffers
+protoc -I $PROTO_DIR --cpp_out=$SRC_DIR $PROTO_DIR/$PROTO_FILE
+
+# Mover arquivos de cabeçalho para o diretório 'include'
+mv $SRC_DIR/*.h $INCLUDE_DIR/
+
+echo "Geração de código concluída."
