a86ebf2122d59e6535dcb35eae1be0026442239b
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-09-14 11:03:09 -0300

    mensagems custom colorida mais facil

---
+++ cpp/messages.hpp
@@ -0,0 +1,41 @@
+#ifndef B7531CEB_511A_44BE_AF37_C958B309142F
+#define B7531CEB_511A_44BE_AF37_C958B309142F
+#include <iostream>
+#include <string>
+
+namespace messages {
+// Códigos de escape ANSI para cores
+const std::string BLUE = "\033[1;34m";
+const std::string RED = "\033[1;31m";
+const std::string YELLOW = "\033[1;33m";
+const std::string RESET = "\033[0m";
+
+enum class Level { INFO, WARNING, ERROR };
+
+std::string color_message(const std::string &message, Level level);
+}  // namespace messages
+
+
+#endif /* B7531CEB_511A_44BE_AF37_C958B309142F */
+
+#include "messages.hpp"
+
+namespace messages {
+std::string color_message(const std::string &message, Level level) {
+  std::string final_message;
+
+  switch (level) {
+    case Level::INFO:
+      final_message = BLUE + "[INFO] " + RESET + message;
+      break;
+    case Level::WARNING:
+      final_message = YELLOW + "[WARNING] " + RESET + message;
+      break;
+    case Level::ERROR:
+      final_message = RED + "[ERROR] " + RESET + message;
+      break;
+  }
+
+  return final_message;
+}
+}  // namespace messages
