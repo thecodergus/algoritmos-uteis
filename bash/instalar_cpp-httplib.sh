f17589bbc867bb508a5276d71d85534a36c6ec1e
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-11-21 09:35:42 -0300

    Faltou entrar na pasta

--- bash/instalar_cpp-httplib.sh
+++ bash/instalar_cpp-httplib.sh
@@ -5,6 +5,7 @@ wget https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.14.1.tar.gz
 tar -xvzf v0.14.1.tar.gz
 cd cpp-httplib-0.14.1
 meson setup build
+cd build
 ninja
 sudo ninja install
 sudo ldconfig
